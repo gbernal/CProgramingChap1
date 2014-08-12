6b23e8ad90319dbde516bd4307c648cc5af8f4a2
Guillermo Bernal <gbernal@Guillermos-MacBook-Pro.local>
2014-08-12 11:36:11 -0400

    Initial Commit

---
+++ CProgramingChap1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CProgramingChap1
+//
+//  Created by Guillermo Bernal on 8/12/14.
+//  Copyright (c) 2014 tuto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
